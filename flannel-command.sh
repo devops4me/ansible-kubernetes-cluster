3e0d2595766b274a83306fef704f5f96ca0add01
Apollo Akora <apolloakora@gmail.com>
2021-03-05 20:50:25 +0000

    New set of do and rollback playbooks.

--- flannel-command.sh
+++
@@ -1,6 +0,0 @@
-Deploy a pod network
-
-Next step is to deploy a pod network. The pod network is used for communication between hosts and is necessary for the Kubernetes cluster to function properly. For this we will use the Flannel pod network. Issue the following two commands on the master node:
-
-kubernetes-master:~$ kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-kubernetes-master:~$ kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
